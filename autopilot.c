e6b6b9c97ffeea0832dbde671c953d79c7f4aacf
Tara Eicher <tarabyte@bitbucket.com>
2015-03-28 22:39:26 -0500

    I removed the functionality of shared.h and moved it to autopilot.h. I also added the correct code for the sensors in autopilot.c, but I have not yet tested the functionality because I need a configuration with 6 sensors. Currently, the turnLeft, turnRight, scootLeft, scootRight, and goStraight functions in autopilot.c are not fully functional. I need to discuss with Christian how to send the info he needs to program the motors as desired.

--- autopilot.c
+++ autopilot.c
@@ -1,26 +1,41 @@
 #include <math.h>
-#include <servo.h>
-#include "shared.h"
 #define MARGIN_OF_ERROR 1
 #define MAX_FRONT_DIST 10
 #define MAX_SIDE_DIST 10
 #define MIN_FRONT_DIST 2
 #define MIN_SIDE_DIST 2
 #define ROBOT_WIDTH 12
-#define WHEEL_RADIUS 2
 #define DEFAULT_SPEED 180
-//
 
-float UltraSensors[] = new float[6];
-float InfraSensors[] = new float[6];
+State state = stopped;
+const float WHEEL_RADIUS = 0.695; //1.75 cm
 
-void choose()
+//array of infrared sensor pins
+	
+const int infraredSensorPins[6] = {A0, A1, A2, ?, ?, ?};
+
+//array of init and echo pins for ultrasonic sensors
+
+const int echoPinUltraSensors[6] = {2, 6, 8, 12, ?, ?};
+const int initPinUltraSensors[6] = {3, 7, 9, 13, ?, ?};
+
+//Arrays of distances for sensors.
+
+int UltraSensorDists[] = new int[6];
+int InfraSensorDists[] = new int[6];
+
+
+int main()
 {
+	//Initialize the sensors.
+	
+	initSensors();
+	
 	//Execute forever until robot stops (indicating error).
 	
 	do
 	{
-		pingAll(UltraSensors, InfraSensors);
+		pingAll();
 	
 		//Check for sensor errors.
 	
@@ -38,7 +53,8 @@ void choose()
 		//If there is no wall in front and the robot is turning, go straight.
 		//If the robot is going straight and is too close to the side, turn slightly and straighten out again. 
 		
-		if((UltraSensors[front_l] < MAX_FRONT_DIST && UltraSensors[front_r] < MAX_FRONT_DIST) && (UltraSensors[front_l] > MIN_FRONT_DIST || UltraSensors[front_l] > MIN_FRONT_DIST))
+		if((UltraSensors[front_l] < MAX_FRONT_DIST && UltraSensors[front_r] < MAX_FRONT_DIST) &&
+		(UltraSensors[front_l] > MIN_FRONT_DIST || UltraSensors[front_l] > MIN_FRONT_DIST))
 		{
 			/*Determine whether there is a gradient in the wall and the direction of the gradient.
 			 *Turn in the corresponding direction.
@@ -51,13 +67,13 @@ void choose()
 				if(UltraSensors[right_f] > MAX_FRONT_DIST || UltraSensors[right_f] >= UltraSensors[right_b])
 				{
 					printf("Turning right...\n");
-					turnRight(UltraSensors[front_r]-UltraSensors[front_l])
+					//turnRight(UltraSensors[front_r]-UltraSensors[front_l])
 					state = turning;
 				}
 				else
 				{
 					printf("Robot trapped. Stopping...\n");
-					stop();
+					//stop();
 					state = stopped;
 				}
 			}
@@ -68,13 +84,13 @@ void choose()
 				if(UltraSensors[left_f] > MAX_SIDE_DIST || UltraSensors[left_f] > UltraSensors[left_b])
 				{
 					printf("Turning left...\n");
-					turnLeft(UltraSensors[front_l]-UltraSensors[front_r])
+					//turnLeft(UltraSensors[front_l]-UltraSensors[front_r])
 					state = turning;
 				}
 				else
 				{
 					printf("Robot trapped. Stopping...\n");
-					stop();
+					//stop();
 					state = stopped;
 				}
 			}
@@ -87,19 +103,19 @@ void choose()
 				 if(UltraSensors[left_f] > MAX_SIDE_DIST)
 				 {
 					printf("Turning left...\n");
-					turnLeft(0);
+					//turnLeft(0);
 					state = turning;
 				 }
 				 else if(UltraSensors[right_f] > MAX_SIDE_DIST)
 				 {
 					printf("Turning right...\n");
-					turnRight(0);
+					//turnRight(0);
 					state = turning;
 				 }
 				 else
 				 {
 					printf("Robot trapped. Stopping...\n");
-					stop();
+					//stop();
 					state = stopped;
 				 }
 			}
@@ -107,43 +123,62 @@ void choose()
 		else if(state == turning)
 		{
 			printf("Going straight...\n");
-			goStraight();
+			//goStraight();
 			state = going_straight;
 		}
 		else if(UltraSensors[left_f] < MIN_SIDE_DIST)
 		{
 			printf("Too close! Moving to the right...\n");
-			scootRight();
+			//scootRight();
 		}
 		else if(UltraSensors[right_f] < MIN_SIDE_DIST)
 		{
 			printf("Too close! Moving to the left...\n");
-			scootLeft();
+			//scootLeft();
 		}
 		
-		//Wait 1 ms before continuing.
+		//Wait 50 ms before continuing.
 		
-		delay(1);
+		delay(50);
 	}
 	while(state != stopped)
+	return 0;
+}
+
+//Initialize sensors.
+
+void initSensors()
+{
+	//Set up the ultrasonic sensors.
+	
+	int i = 0;
+	for(; i < initPinUltraSensors.length; i++) pinMode(initPinUltraSensors[i], INPUT);
+	int j = 0;
+	for(; j < echoPinUltraSensors.length; j++) pinMode(echoPinUltraSensors[j], OUTPUT);
 }
 
 //Ping all sensors.
 
-void pingAll(float[] sensors1, float[] sensors2)
+void pingAll()
 {
-	for(i=0; i < sensors1.length; i++)
-	{
-		pinMode(sensors[i].init, OUTPUT);
-  		pinMode(sensors[i].echo, INPUT);
-	}
-	for(i=0; i < sensors2.length; i++)
-	{
-		pinMode(sensors[i].init, OUTPUT);
-  		pinMode(sensors[i].echo, INPUT);
-	}
+	int i = 0;
+	int j = 0;
+	for(; i < infras.length; i++)	InfraSensorDists[i] = analogRead(InfraredSensorPins[i]);
+	for(; j < ultras.length; j++)	UltraSensorDists[j] = getDistance(initPinUltraSensors[j], echoPinUltraSensors[j]);
+ 
 }
 
+//Helper function to get distance from ultrasonic sensors.
+
+int getDistance (int initPin, int echoPin)
+{
+	digitalWrite(initPin, HIGH);
+	delayMicroseconds(10); 
+	digitalWrite(initPin, LOW); 
+	unsigned long pulseTime = pulseIn(echoPin, HIGH); 
+	int distance = pulseTime/58;
+	return distance;
+}
 //Send sensor signal to turn right based on a certain gradient.
 
 void turnRight(float gradient)
@@ -154,7 +189,7 @@ void turnRight(float gradient)
 	motors[motor_back_l].write(DEFAULT_SPEED / WHEEL_RADIUS);
 }
 
-//Send sensor signal to turn right based on a certain gradient.
+//Send sensor signal to turn left based on a certain gradient.
 
 void turnLeft(float gradient)
 {
@@ -190,6 +225,8 @@ void scootRight()
 {
 	turnRight(ROBOT_WIDTH / 2);
 	wait(5);
+	turnLeft(ROBOT_WIDTH / 2);
+	wait(5);
 	goStraight();
 }
 
@@ -198,6 +235,8 @@ void scootRight()
 void scootLeft()
 {
 	turnLeft(ROBOT_WIDTH / 2);
+	wait(5);
+	turnRight(ROBOT_WIDTH / 2);
 	wait(5);
 	goStraight();
 }
